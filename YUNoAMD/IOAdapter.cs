12ec0d27a0761982502429bd25466a6ee53caf50
fschwiet <fschwiet@gmail.com>
2012-01-29 11:22:13 -0800

    starting porting concoct..  might need variable number of arguments from Jurassic

---
+++ YUNoAMD/IOAdapter.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace YUNoAMD
+{
+    public class IOAdapter
+    {
+        public void print(string message)
+        {
+            
+        }
+
+        public void warn(string message, int line, string source, int column)
+        {
+            
+        }
+
+        public string readFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- YUNoAMD/RequireJsCompiler.cs
+++ YUNoAMD/RequireJsCompiler.cs
@@ -7,7 +7,32 @@ namespace YUNoAMD
     {
         public string Compile(string appPath)
         {
+            var jsEngine = new Jurassic.ScriptEngine();
+
+            jsEngine.Evaluate(LoadResource("require.js"));
+            jsEngine.Evaluate(LoadResource("json2.js"));
+            jsEngine.Evaluate(LoadResource("adapt/rhino.js"));
+
+            jsEngine.Evaluate(@"require({ baseUrl:'http://requirejs.resources/'});");
+
+            var ioAdapter = new IOAdapter();
+
+            jsEngine.SetGlobalFunction("print", new Action<string>(s => {}));
+
+
             return File.ReadAllText(appPath);
+        }
+
+        private string LoadResource(string path)
+        {
+            var assembly = this.GetType().Assembly;
+
+            using(var reader = assembly.GetManifestResourceStream(this.GetType().Namespace + ".requireJS." + path))
+            {
+                
+            }
+
+            return "";
         }
     }
 }
