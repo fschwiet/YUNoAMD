9669d635b69baf78c057a56bb10b7abf9419259c
fschwiet <fschwiet@gmail.com>
2012-01-30 21:34:51 -0800

    starting a test for load module...

---
+++ YUNoAMD.Test/Can_build_requireJS_environment/defines_module_load.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace YUNoAMD.Test.Can_build_requireJS_environment
+{
+    public class defines_module_load : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
